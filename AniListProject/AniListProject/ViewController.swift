93980a498483e473ceb29ea61035c6e5448f1875
juan <juan.arcos.hdz@gmail.com>
2020-01-11 12:34:36 -0600

    AniList initial Project

---
+++ AniListProject/AniListProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AniListProject
+//
+//  Created by juan on 1/11/20.
+//  Copyright © 2020 jarcos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
